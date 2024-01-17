1f213cfd7a4cc1e8a823a57371554ff73ae0bfc5
Kaspooky <kd352@Njit.edu>
2024-01-16 21:47:08 -0500

    initial work on parsing file names

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import os
+file = open("testfile", "w")
 for entries in os.listdir(r'C:\Users\Kacper\Downloads'):
-    print(entries)
+    splitEntry = entries.split(".")
+    if (splitEntry[-1] == "txt"):
+        file.write(entries + "\n")
+file.close()
