da4480b22111fe998959bceab58e048e80cff2e4
Kaspooky <kd352@Njit.edu>
2024-02-15 12:18:14 -0500

    Rewrote basic functionality

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
-file = open("testfile", "w")
+import shutil
+DesktopPath = 'C:\\Users\\Kacper\\Desktop'
+downloadPath = 'C:\\Users\\Kacper\\Downloads'
+
+file = open("moveLog.txt", "a")
 for entries in os.listdir(r'C:\Users\Kacper\Downloads'):
     splitEntry = entries.split(".")
     if (splitEntry[-1] == "txt"):
-        file.write(entries + "\n")
+        shutil.move(downloadPath + '\\' + entries,
+                    DesktopPath + '\\' + splitEntry[-1])
+        file.write("file: " + entries + " from: " + downloadPath +
+                   " --> " + DesktopPath + '\\' + splitEntry[-1] + "\n")
 file.close()
